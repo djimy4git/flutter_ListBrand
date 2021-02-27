52c62d98e6310cc0649093322b4a7eb34a52eb3e
Djamel Boudissa <djimy213@gmail.com>
2021-02-27 18:19:12 +0100

    first upadate

---
+++ lib/homepage.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import './listbrandcar.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Car Brand List'),
+        ),
+        body: ListBrandCar(),
+      ),
+    );
+  }
+}
